42937a5b03b6447937c7ef4e14df90a9faf66447
Kevin98 <kevinryner@yahoo.co.uk>
2021-08-31 11:29:18 +0100

    associations set up

--- models/index.js
+++ models/index.js
@@ -6,11 +6,25 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
+
 // Categories have many Products
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongsToMany(Tags, {through: 'ProductTag', foreignKey: 'product_id'});
+
+
 // Tags belongToMany Products (through ProductTag)
+
+Tags.belongsToMany(Product, {through: 'ProductTag', foreignKey: 'tag_id'});
 
 module.exports = {
   Product,
