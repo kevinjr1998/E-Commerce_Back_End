aa3d499471b8bfb4557ecbbdb6afb703f751080d
Kevin98 <kevinryner@yahoo.co.uk>
2021-08-31 11:09:40 +0100

    productTag comums set up

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,6 +6,20 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id:{
+      type: DataTypes.INTEGER,
+      // references product(id)
+    },
+    tag_id:{
+      type: DataTypes.INTEGER,
+      //references tag(id)
+    }
     // define columns
   },
   {
